db002c0c691c3366e51ef51353d40a565f992359
Xamez <34zemax34@gmail.com>
2023-02-02 17:58:24 +0100

    :pencil2: Deleting an outdated comment

--- src/main/kotlin/fr.xamez/annotation/Element.kt
+++ src/main/kotlin/fr.xamez/annotation/Element.kt
@@ -1,10 +1,4 @@
 package fr.xamez.annotation
 
 @Retention(AnnotationRetention.RUNTIME)
-/**
- * Annotation used to define the name of the element
- * @param name the name of the element
- * @param inline if the element need an opening and closing tag
- *        (ex: '<rect>...</rect>' or '')
- */
 annotation class Element(val name: String)
